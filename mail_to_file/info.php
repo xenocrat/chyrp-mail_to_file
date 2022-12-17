ab2d00aab834a8cf6b1dc0184937420c47d52624
Daniel Pimley <dan@pimley.net>
2022-12-17 14:00:53 +0000

    Fixed version string

--- mail_to_file/info.php
+++ mail_to_file/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Mail to File", "mail_to_file"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.0,
+    "version"       => "2023.01",
     "description"   => __("Redirects outgoing emails to a digest file.", "mail_to_file"),
     "author"        => array(
         "name"      => "Daniel Pimley",
